94c093d5e4b4e0f4b7c49711510db098cdcc4ecf
LHF <836138284@qq.com>
2020-03-22 09:13:36 +0800

    新增登录校验filter

---
+++ StudentsGradesSystem/src/cn/cqu/studentsgradessystem/web/filter/LoginFilter.java
@@ -0,0 +1,52 @@
+package cn.cqu.studentsgradessystem.web.filter;
+
+import cn.cqu.studentsgradessystem.domain.Administator;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *完成登录校验，未登录的情况下需要跳转到login页面
+ */
+@WebFilter("/*")
+public class LoginFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        //1.强制类型转换
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        //2.获取uri，判断是否包含登陆相关资源
+        String requestURI = request.getRequestURI();
+        if (requestURI.contains("/login.jsp") ||
+                requestURI.contains("/checkCodeServlet") ||
+                requestURI.contains("/loginServlet") ||
+                requestURI.contains("/css/")||
+                requestURI.contains("/js/")||
+                requestURI.contains("/fonts/")){
+            //资源放行
+            chain.doFilter(request, response);
+        }else{
+            Administator login_admin = (Administator) request.getSession().getAttribute("login_admin");
+            //未放行，进行跳转
+            if (login_admin != null) {
+                //登陆成功,资源放行
+                chain.doFilter(request,response);
+            } else {
+                //登陆失败
+                request.setAttribute("loginFeedback","请先登录！");
+                request.getRequestDispatcher("/login.jsp").forward(request,response);
+            }
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
